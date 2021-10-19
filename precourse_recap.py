1d7fc0d2fa4c49beadeb5ffdf0f7b8831a55dc96
Stefano Binando <sbinando@gmail.com>
2021-10-19 09:28:43 +0100

    changes the second input text; makes answers not case sensitive

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,15 +1,15 @@
-fruits = ["apple", "banana", "cherry"]
+fruits = ["apple", "banana", "cherry", "avocado", "blueberry", "apricot"]
 print()
 print("---------------------------")
 choice = int(input("pick a number between 1 and "+str(len(fruits))+": "))
 print()
-print('I spy, with my little eye, a fruit beginning with... "' + fruits[choice - 1][0] + '"!')
+print("I'm thinking of a fruit beginning with... " + '"' + fruits[choice - 1][0].upper() + '"!')
 print()
 answer = input("and your guess is...? ")
 
 print()
 
-if answer == fruits[choice - 1]:
+if answer.upper() == fruits[choice - 1].upper():
     print("CORRECT!")
 else:
     print("...wrong, sorry... :(")
